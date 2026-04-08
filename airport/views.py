ffc5d26fa8ce004280be40eb5a06fba7415ee109
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-04-08 11:56:21 +0300

    feat: Create airport and booking views

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,91 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from airport.models import (
+    Country,
+    City,
+    Airport,
+    Route,
+    Crew,
+    Airplane,
+    AirplaneType,
+    Flight
+)
+from airport.permissions import IsAdminOrIfAuthenticatedReadOnly
+from airport.serializers import (
+    CountrySerializer,
+    CitySerializer,
+    AirportSerializer,
+    RouteSerializer,
+    CrewSerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    FlightSerializer
+)
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser]
+    )
+    def upload_image(self, request: Request, pk: int = None) -> Response:
+        item = self.get_object()
+        serializer = self.get_serializer(item, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from booking.models import Order, Ticket
+from booking.serializers import OrderSerializer, TicketSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet:
+        return Order.objects.filter(user=self.request.user).prefetch_related("order_tickets")
+
+    def perform_create(self, serializer: OrderSerializer) -> None:
+        serializer.save(user=self.request.user)
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet:
+        return Ticket.objects.filter(order__user=self.request.user).select_related("order", "flight")
+
+    def perform_create(self, serializer: TicketSerializer) -> None:
+        order = serializer.validated_data["order"]
+        if order.user != self.request.user:
+            raise PermissionDenied("You cannot create tickets for another user's order.")
+        serializer.save()
