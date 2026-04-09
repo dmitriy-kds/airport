2892d24b149ff7702fa2097ef726b04b69dc13b0
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-04-09 21:16:43 +0300

    feat: Optimize queryset for OrverViewSet

--- booking/views.py
+++ booking/views.py
@@ -1,34 +1,36 @@
 from django.db.models import QuerySet
 from rest_framework import viewsets
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.serializers import ModelSerializer
 
-from booking.models import Order, Ticket
-from booking.serializers import OrderSerializer, TicketSerializer
+from booking.models import Order
+from booking.serializers import (
+    OrderListSerializer,
+    OrderDetailSerializer,
+    OrderCreateSerializer
+)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self) -> QuerySet:
-        return Order.objects.filter(user=self.request.user).prefetch_related("order_tickets")
+    def get_serializer_class(self) -> type[ModelSerializer]:
+        if self.action == "list":
+            serializer_class = OrderListSerializer
+        elif self.action == "retrieve":
+            serializer_class = OrderDetailSerializer
+        else:
+            serializer_class = OrderCreateSerializer
+        return serializer_class
 
-    def perform_create(self, serializer: OrderSerializer) -> None:
+    def get_queryset(self) -> QuerySet:
+        return (Order.objects.all().filter(user=self.request.user).
+                prefetch_related(
+                    "order_tickets",
+                    "order_tickets__flight",
+                    "order_tickets__flight__route"
+                )
+        )
+
+    def perform_create(self, serializer: OrderCreateSerializer) -> None:
         serializer.save(user=self.request.user)
-
-
-class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self) -> QuerySet:
-        return Ticket.objects.filter(order__user=self.request.user).select_related("order", "flight")
-
-    def perform_create(self, serializer: TicketSerializer) -> None:
-        order = serializer.validated_data["order"]
-        if order.user != self.request.user:
-            raise PermissionDenied("You cannot create tickets for another user's order.")
-        serializer.save()
