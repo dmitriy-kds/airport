0013ecc014d2cda33281dd72f6e7583cc76ab768
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-04-09 22:25:58 +0300

    feat: Move setUp code to core/tests/base.py and import it into model tests

--- airport/tests/test_airport_models.py
+++ airport/tests/test_airport_models.py
@@ -11,12 +11,12 @@ from airport.models import (
     Country,
     City,
     Airport,
-    Route,
     Crew,
     AirplaneType,
     Airplane,
     create_custom_path
 )
+from core.tests.base import BaseAPITestCase
 
 
 class CountryModelTests(TestCase):
@@ -123,41 +123,7 @@ class AirportModelTests(TestCase):
             Airport.objects.create(**data)
 
 
-class RouteModelTests(TestCase):
-    def setUp(self):
-        self.country = Country.objects.create(
-            name="United Kingdom",
-            code="UK"
-        )
-        self.country2 = Country.objects.create(
-            name="Ukraine",
-            code="UA"
-        )
-        self.city = City.objects.create(
-            name="London",
-            country=self.country,
-        )
-        self.city2 = City.objects.create(
-            name="Kyiv",
-            country=self.country2,
-        )
-        self.airport = Airport.objects.create(
-            name="Heathrow",
-            city=self.city,
-            latitude=51.4680,
-            longitude=0.4550,
-        )
-        self.airport2 = Airport.objects.create(
-            name="Boryspil",
-            city=self.city2,
-            latitude=50.345001,
-            longitude=30.894699,
-        )
-        self.route = Route.objects.create(
-            source=self.airport,
-            destination=self.airport2,
-        )
-
+class RouteModelTests(BaseAPITestCase):
     def test_create_route(self):
         self.assertEqual(self.route.source, self.airport)
 
@@ -288,62 +254,7 @@ class AirplaneModelTests(TestCase):
             Airplane.objects.create(**data)
 
 
-class FlightModelTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.country = Country.objects.create(
-            name="United Kingdom",
-            code="UK"
-        )
-        cls.country2 = Country.objects.create(
-            name="Ukraine",
-            code="UA"
-        )
-        cls.city = City.objects.create(
-            name="London",
-            country=cls.country,
-        )
-        cls.city2 = City.objects.create(
-            name="Kyiv",
-            country=cls.country2,
-        )
-        cls.airport = Airport.objects.create(
-            name="Heathrow",
-            city=cls.city,
-            latitude=51.4680,
-            longitude=0.4550,
-        )
-        cls.airport2 = Airport.objects.create(
-            name="Boryspil",
-            city=cls.city2,
-            latitude=50.345001,
-            longitude=30.894699,
-        )
-        cls.route = Route.objects.create(
-            source=cls.airport,
-            destination=cls.airport2,
-        )
-        cls.crew = Crew.objects.create(
-            first_name="Tom",
-            last_name="Cruise",
-        )
-        cls.airplane_type = AirplaneType.objects.create(
-            name="Boeing 737",
-        )
-        cls.airplane = Airplane.objects.create(
-            name="Spirit Airlines N832AS",
-            rows=30,
-            seats_in_row=6,
-            airplane_type=cls.airplane_type,
-        )
-        cls.flight = Flight.objects.create(
-            route=cls.route,
-            airplane=cls.airplane,
-            departure_time="2024-04-15T14:30:00",
-            arrival_time="2024-04-15T18:30:00",
-        )
-        cls.flight.crew.add(cls.crew)
-
+class FlightModelTests(BaseAPITestCase):
     def test_create_flight(self):
         self.assertEqual(self.flight.route, self.route)
 
